5971f2fe0b4acde54adac7e9681d01beaac9a474
Ch06044 <ch06044@ues.edu.sv>
2024-04-19 08:58:05 -0600

    My first commit

---
+++ Proyecto/CH06044.hs
@@ -0,0 +1,8 @@
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+main :: IO ()
+main = do
+    let num = 5
+    putStrLn ("El factorial de " ++ show num ++ " es " ++ show (factorial num))

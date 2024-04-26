47f3d176cc5f4ace981c2445a829f95409bdc544
Ch06044 <ch06044@ues.edu.sv>
2024-04-25 21:32:04 -0600

    My first commit

---
+++ Proyecto/HiGit.hs
@@ -0,0 +1,9 @@
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+main :: IO ()
+main = do
+    let num = 5
+    putStrLn ("Bienvenido El factorial de " ++ show num ++ " es " ++ show (factorial num))
+    
